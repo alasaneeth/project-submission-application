f870b0791378c39a7f97cad37e32d53f60d5304a
alasaneeth <alasaneeth22@gmail.com>
2025-09-28 10:16:52 +0300

    Add: Configure Routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { SubmitProjectComponent } from './pages/submit-project/submit-project.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+   {
+    path:'dashboard',
+    component:DashboardComponent
+  },
+  {
+    path:'students',
+    component:StudentsComponent
+  },
+  {
+    path:'submitProject',
+    component:SubmitProjectComponent
+  },
+   {
+    path:'all-project',
+    component:ProjectsComponent
+  }
+];
+
+
